de710cf347d7b7803a70bf7ee3bee8fbd11b74ad
team7 <hnhyerim@gmail.com>
2023-09-09 01:30:25 +0900

    Style: 코드 정리

--- src/server.js
+++ src/server.js
@@ -5,10 +5,10 @@ const app = express()
 app.set('view engine', 'pug')
 app.set('views', __dirname + '/views')
 app.use('/public', express.static(__dirname + '/public'))
-app.get('/', (req, res) => res.render('home'))
+app.get('/', (_, res) => res.render('home'))
 
 // home만 사용할 것이기 때문에 redirect를 설정해줬다.
-app.get('/*', (req, res) => res.render('home'))
+app.get('/*', (_, res) => res.render('home'))
 
 const handleListen = () => console.log(`Listening on http://localhost:3000/`)
 app.listen(3000, handleListen)
