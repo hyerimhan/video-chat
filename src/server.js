618cec0a9e15b62280ec3d1a65f980fb3e9e5cbf
team7 <hnhyerim@gmail.com>
2023-09-10 01:46:21 +0900

    Feat: WebSockets in Node.js

--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
+import http from 'http'
+import WebSocket from 'ws'
 import express from 'express'
 
 const app = express()
@@ -11,4 +13,11 @@ app.get('/', (_, res) => res.render('home'))
 app.get('/*', (_, res) => res.render('home'))
 
 const handleListen = () => console.log(`Listening on http://localhost:3000/`)
-app.listen(3000, handleListen)
+
+// http 서버 생성
+const server = http.createServer(app)
+// webSocket 서버 생성
+// http 서버와 ws 2개의 protocol 사용 가능. http 서버 위에 ws서버를 만들기 위함. http, ws가 같은 port에 있길 원할때 사용 (선택)
+// 이렇게 하는 이유는 위에 설정해 둔 views, static files, home, redirection을 원하기 때문
+const wss = new WebSocket.Server({ server })
+server.listen(3000, handleListen)
