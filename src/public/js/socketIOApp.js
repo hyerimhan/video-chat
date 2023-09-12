aed54d6b024828ad19aaa9b255ff63e21d6ef7bf
team7 <hnhyerim@gmail.com>
2023-09-12 23:59:20 +0900

    Feat: Room Notifications

--- src/public/js/socketIOApp.js
+++ src/public/js/socketIOApp.js
@@ -15,11 +15,22 @@ function addMessage(message) {
   ul.appendChild(li)
 }
 
+function handleMessageSubmit(event) {
+  event.preventDefault()
+  const input = room.querySelector('input')
+  socket.emit('new_message', input.value, roomName, () => {
+    addMessage(`You: ${input.value}`)
+    input.value = ''
+  })
+}
+
 function showRoom() {
   welcome.hidden = true
   room.hidden = false
   const h3 = room.querySelector('h3')
   h3.innerText = `Room ${roomName}`
+  const form = room.querySelector('form')
+  form.addEventListener('submit', handleMessageSubmit)
 }
 
 function handleRoomSubmit(event) {
@@ -28,12 +39,15 @@ function handleRoomSubmit(event) {
   // websocket에서 서버로 보낼 때 사용했던 "send" 대신 emit을 사용한다.
   // 첫번째 argument에는 event이름, 두번째 보내고 싶은 payload, 세번째 서버에서 호출하는 function 등 원하는만큼 argument를 사용할 수 있다. (websocket은 string만 가능)
   // 단, function은 emit의 마지막 argument 여야 한다.
-  socket.emit('enter_room', { payload: input.value }, showRoom)
+  socket.emit('enter_room', input.value, showRoom)
   roomName = input.value
   input.value = ''
 }
 
 form.addEventListener('submit', handleRoomSubmit)
 
-// 방에 다른 유저들이 입장하면 전체 메세지로 알려줌
+// 방에 다른 유저들이 입장 or 퇴장하면 전체 메세지로 알려줌
 socket.on('welcome', () => addMessage('Someone joined!'))
+socket.on('bye', () => addMessage('Someone left ㅠㅠ'))
+
+socket.on('new_message', addMessage)
--- src/socketIOServer.js
+++ src/socketIOServer.js
@@ -21,11 +21,22 @@ wsServer.on('connection', (socket) => {
   // 모든 event를 감시
   socket.onAny((event) => console.log(`Socket Event: ${event}`))
   socket.on('enter_room', (roomName, done) => {
-    socket.join(roomName.payload)
-    // app.js emit 세번째 argument인 서버에서 호출하는 function
+    socket.join(roomName)
+    // app.js emit 마지막 argument인 서버에서 호출하는 function (FE에서 코드 실행)
     done()
     // 방에 다른 유저들이 입장하는지 확인
-    socket.to(roomName.payload).emit('welcome')
+    socket.to(roomName).emit('welcome')
+  })
+
+  // 유저가 접속을 중단할 것이지만 아직 방을 완전히 나가지는 않은 상태
+  socket.on('disconnecting', () =>
+    socket.rooms.forEach((room) => socket.to(room).emit('bye'))
+  )
+
+  // 참여한 방을 확인하고 새로운 메세지를 전달한다.
+  socket.on('new_message', (message, roomName, done) => {
+    socket.to(roomName).emit('new_message', message)
+    done()
   })
 })
 
