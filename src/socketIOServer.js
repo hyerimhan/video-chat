e371228b6a52910da3c04f919b67126bd3547b7f
team7 <hnhyerim@gmail.com>
2023-09-12 16:27:53 +0900

    Feat: SockerIO vs WebSockets

---
+++ src/socketIOServer.js
@@ -0,0 +1,25 @@
+import http from 'http'
+import SocketIO from 'socket.io'
+import express from 'express'
+
+const app = express()
+
+app.set('view engine', 'pug')
+app.set('views', __dirname + '/views')
+app.use('/public', express.static(__dirname + '/public'))
+app.get('/', (_, res) => res.render('socketIOHome'))
+
+// home만 사용할 것이기 때문에 redirect를 설정해줬다.
+app.get('/*', (_, res) => res.render('socketIOHome'))
+
+// http 서버 생성
+const httpServer = http.createServer(app)
+// SocketIO 서버 생성
+const wsServer = SocketIO(httpServer)
+
+wsServer.on('connection', (socket) => {
+  console.log(socket)
+})
+
+const handleListen = () => console.log(`Listening on http://localhost:3000/`)
+httpServer.listen(3000, handleListen)
--- src/webSocketServer.js
+++ src/webSocketServer.js
@@ -7,26 +7,24 @@ const app = express()
 app.set('view engine', 'pug')
 app.set('views', __dirname + '/views')
 app.use('/public', express.static(__dirname + '/public'))
-app.get('/', (_, res) => res.render('home'))
+app.get('/', (_, res) => res.render('webSocketHome'))
 
 // home만 사용할 것이기 때문에 redirect를 설정해줬다.
-app.get('/*', (_, res) => res.render('home'))
-
-const handleListen = () => console.log(`Listening on http://localhost:3000/`)
+app.get('/*', (_, res) => res.render('webSocketHome'))
 
 // http 서버 생성
-const server = http.createServer(app)
+const httpServer = http.createServer(app)
 // webSocket 서버 생성
 // http 서버와 ws 2개의 protocol 사용 가능. http 서버 위에 ws서버를 만들기 위함. http, ws가 같은 port에 있길 원할때 사용 (선택)
 // 이렇게 하는 이유는 위에 설정해 둔 views, static files, home, redirection을 원하기 때문
-const wss = new WebSocket.Server({ server })
-server.listen(3000, handleListen)
+const wsServer = new WebSocket.Server({ httpServer })
 
 // fake database
+// 다른 브라우저 환경을 담을 빈 배열
 const sockets = []
 
-// server.js의 socket은 연결된 어떤 사람 (연결된 브라우저와의 contact(연락)라인)
-wss.on('connection', (socket) => {
+// webSocketServer.js의 socket은 연결된 어떤 사람 (연결된 브라우저와의 contact(연락)라인)
+wsServer.on('connection', (socket) => {
   sockets.push(socket)
   socket['nickname'] = 'Anon'
   console.log('Connected to Browser ✅')
@@ -43,3 +41,6 @@ wss.on('connection', (socket) => {
     }
   })
 })
+
+const handleListen = () => console.log(`Listening on http://localhost:3000/`)
+httpServer.listen(3000, handleListen)
