2ad699928f74106bc9f3e95e7e67dc155f101c79
hyerimhan <hnhyerim@gmail.com>
2023-09-14 17:06:46 +0900

    Feat: Rooms

--- src/webRTCServer.js
+++ src/webRTCServer.js
@@ -17,5 +17,13 @@ const httpServer = http.createServer(app)
 // SocketIO 서버 생성
 const wsServer = SocketIO(httpServer)
 
+wsServer.on('connection', (socket) => {
+  socket.on('join_room', (roomName, done) => {
+    socket.join(roomName)
+    done()
+    socket.to(roomName).emit('welcome')
+  })
+})
+
 const handleListen = () => console.log(`Listening on http://localhost:3000/`)
 httpServer.listen(3000, handleListen)
